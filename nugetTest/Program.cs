f6b7689e45f48a7d92c2e7771bd7930fdc382451
Mike <Mike@DESKTOP-7DT4NS2>
2018-01-28 00:04:09 +0200

    first commit

---
+++ nugetTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace nugetTest
+{
+    public class MyTestClasss
+    {
+
+        public void DownloadAsyncTask()
+        {
+            Console.WriteLine("Hello from packege");
+            //HttpClient httpClient = new HttpClient();
+            //await httpClient.GetStringAsync("Hi From Package");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
